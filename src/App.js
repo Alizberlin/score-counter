2da51b6d018b174006b9810cacd42ed13d192880
Ali Raza <mrakaraza@gmail.com>
2023-12-07 15:21:06 +0100

    no

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import { Times } from './components/Times';
 import { Stopwatch } from './components/Timer';
+import  NoSleep  from "nosleep.js";
+import { useEffect } from 'react';
 function App() {
+  useEffect(() => {
+    let isEnableNoSleep = false;
+    const noSleep = new NoSleep();
+    document.addEventListener(
+      `click`,
+      function enableNoSleep() {
+        document.removeEventListener(`click`, enableNoSleep, false);
+        noSleep.enable();
+        isEnableNoSleep = true;
+        alert(`click and enable noSleep`);
+      },
+      false
+    );
+    return () => {
+      if (isEnableNoSleep) {
+        noSleep.disable();
+      }
+    };
+  }, []);
   return (
     <div className="App font-mono">
       <Times></Times>
